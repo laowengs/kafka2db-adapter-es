5a90ae47a4d386b84a73dd0ba68e9b99a8b857a6
laowengs <229595412@qq.com>
2023-07-01 20:21:17 +0800

    接入apolo

--- src/main/java/com/laowengs/kafka2db/adapter/es/Kafka2dbAdapterEsApplication.java
+++ src/main/java/com/laowengs/kafka2db/adapter/es/Kafka2dbAdapterEsApplication.java
@@ -1,11 +1,13 @@
 package com.laowengs.kafka2db.adapter.es;
 
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
 @EnableKafka
+@EnableApolloConfig
 public class Kafka2dbAdapterEsApplication {
 
     public static void main(String[] args) {
